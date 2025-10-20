cf04dfb4dca003a0ec838edd328d663b37c6a728
Alex Lang <alexlangbusiness80809@gmai.com>
2025-10-20 21:35:19 +0000

    creates view for flight details

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import models
 
 def index(request):
     """
@@ -10,3 +11,17 @@ def index(request):
         'tagline': 'Streamline Your Workflow, Amplify Your Productivity',
     }
     return render(request, 'home/index.html', context)
+
+#View display for flight display feature
+def flight_details(request, flight_id)
+
+#Grabs flight from database, returns error if entered flight does not exist
+flight = get_object_or_404(Flight, id=flight_id)
+
+#Passes flight to the template
+context = {
+        'flight': flight,
+    }
+
+#Runs the template with the flight data 
+return render(request, 'home/flight_detail.html', context)
