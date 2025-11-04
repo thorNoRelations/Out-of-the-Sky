bc332520b19fcf52ec85fe759e31886673f13c41
Enzo K <131043112+seacactus13@users.noreply.github.com>
2025-11-03 18:38:05 -0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('flight/<int:flight_id>/', views.flight_detail, name='flight_detail'),
+    path('weather/', views.weather_insights, name='weather_insights'),
 ]
