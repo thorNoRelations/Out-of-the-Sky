5753363e406a958cef8aa169c3195f5d3570d803
Enzo K <131043112+seacactus13@users.noreply.github.com>
2025-11-06 15:25:55 -0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('flight/<int:flight_id>/', views.flight_detail, name='flight_detail'),
     path("admin/api-usage/", api_usage_readout, name="api_usage_readout"),
+    path('weather/', views.weather_insights, name='weather_insights'),
 ]
