ebe64e2acc5896a1e14cf0c11e9cb15fe2007c6d
Mason Sowanick <msowanic@uccs.edu>
2025-11-13 17:41:28 -0700

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,7 +34,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['out-of-the-sky.onrender.com']
+ALLOWED_HOSTS = [
+    'out-of-the-sky.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
